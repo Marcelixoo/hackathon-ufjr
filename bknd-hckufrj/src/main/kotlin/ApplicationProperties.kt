5005b8454b619ccfd5f49337e07998c314ce3c8e
Gustavo Jasponde <gustavocalonico@gmail.com>
2017-08-30 02:25:32 -0300

    Remove kotlin stuff.

--- bknd-hckufrj/src/main/kotlin/ApplicationProperties.kt
+++
@@ -1,8 +0,0 @@
-package bkndhckufrj
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties("app")
-class BkndApplicationProprerties{
-    var appName : String? = null
-}
